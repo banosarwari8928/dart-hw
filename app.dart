fcde710255e186f031ce634972816480169d3a1d
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-12 14:08:04 +0430

    dart

--- app.dart
+++ app.dart
@@ -3,8 +3,17 @@ void main(){
     //     print(a);
     // }
     num result=0;
-    for(int i =1;i<=50;i++){
+    // for(int i =1;i<=50;i++){
+    //     result+=i;
+    // }
+    // for(int i =13;i<=50;i+=2){
+    //     result+=i;
+    // }   
+    for(int i =13;i<=50;i++){
+        if(i==2){
+            continue;
+        }
         result+=i;
-    }
+    }  
 print(i);
 }
