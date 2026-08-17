349c3510cf569915ce37efa5eef36bb7193d73b9
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-17 14:39:05 +0430

    first last function of list

--- app.dart
+++ app.dart
@@ -1,24 +1,27 @@
 void main(){
-    for (int a =1; a<=10; a++){
-        print(a);
-    }
-    num result=0;
-    for(int i =1;i<=50;i++){
-        result+=i;
-    }
-    for(int i =13;i<=50;i+=2){
-        result+=i;
-    }   
-    for(int i =13;i<=50;i++){
-        if(i==2){
-            continue;
-        }
-        result+=i;
-    }  
-print(i);
+//     for (int a =1; a<=10; a++){
+//         print(a);
+//     }
+//     num result=0;
+//     for(int i =1;i<=50;i++){
+//         result+=i;
+//     }
+//     for(int i =13;i<=50;i+=2){
+//         result+=i;
+//     }   
+//     for(int i =13;i<=50;i++){
+//         if(i==2){
+//             continue;
+//         }
+//         result+=i;
+//     }  
+// print(i);
 // hw of 17 aug
 List <int> numbers=[1,2,3,10,11,12,13];
-print(numbers.length);
-print(numbers.isEmpty);
-print(numbers.isNotEmpty);
+// print(numbers.length);
+// print(numbers.isEmpty);
+// print(numbers.isNotEmpty);
+print(numbers[0]);
+print(numbers.first);
+print(numbers.last);
 }
