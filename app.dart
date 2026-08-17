70f7e23c6a7ef04cb5147e19d48cd842a4349eda
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-17 14:35:33 +0430

    length of list

--- app.dart
+++ app.dart
@@ -1,14 +1,14 @@
 void main(){
-    // for (int a =1; a<=10; a++){
-    //     print(a);
-    // }
+    for (int a =1; a<=10; a++){
+        print(a);
+    }
     num result=0;
-    // for(int i =1;i<=50;i++){
-    //     result+=i;
-    // }
-    // for(int i =13;i<=50;i+=2){
-    //     result+=i;
-    // }   
+    for(int i =1;i<=50;i++){
+        result+=i;
+    }
+    for(int i =13;i<=50;i+=2){
+        result+=i;
+    }   
     for(int i =13;i<=50;i++){
         if(i==2){
             continue;
@@ -16,4 +16,6 @@ void main(){
         result+=i;
     }  
 print(i);
+List <int> numbers=[1,2,3,10,11,12,13];
+print(numbers.lenght);
 }
