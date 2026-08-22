f21cc9f5fba3e72a9f2805d79cc976cdd6269b90
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-22 12:01:19 +0430

    map worked on list

--- app.dart
+++ app.dart
@@ -1,5 +1,7 @@
 void main(){
      List<num> prices=[1200,203,400,210,490,];
  List<num>currntPrice = prices.map((price)=>price-(price*10)/100).toList();
+ currntPrice.where((x)=>x>800).toList();
 print(currntPrice);
+
 }
