eb2af4ad2a12e51334d99d01efff8d91683ea289
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-23 13:18:09 +0430

    Sublist

--- app.dart
+++ app.dart
@@ -1,6 +1,12 @@
 void main(){
     List<num> numbers=[5,3,4,6,8];
     // numbers.fillRange(1,4,6);
-    print(numbers);
-    numbers.setRange(1,3,[5,6]);
+    // numbers.setRange(1,3,[5,6]);
+    numbers.json('_');       
+   var newNumbers=numbers.asMap();
+
+//    print(newNumber);
+    // print(numbers);
+List<String> listStudents=["Ahmad","Salim","Wahid","Reza","Mujtaba","Murtaza"];
+ List<String> FilteredStudents= listStudents.subList(1,4);
 }
