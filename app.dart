1c846e2477e21da0ad16e2847d87f71a89275c4e
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-25 12:51:34 +0430

    map data type

--- app.dart
+++ app.dart
@@ -1,12 +1,11 @@
 void main(){
-    List<num> numbers=[5,3,4,6,8];
-    // numbers.fillRange(1,4,6);
-    // numbers.setRange(1,3,[5,6]);
-    numbers.json('_');       
-   var newNumbers=numbers.asMap();
-
-//    print(newNumber);
-    // print(numbers);
-List<String> listStudents=["Ahmad","Salim","Wahid","Reza","Mujtaba","Murtaza"];
- List<String> FilteredStudents= listStudents.subList(1,4);
-}
+   Map<String,num> Students={
+    "Ali":50,
+    "Murtaza":60,
+    "Mujtaba":76,
+    "Mohammad":88,
+    "Akbar":92,
+    "Salim":98
+   };
+   print(scores["Murtaza"]);
+   }
