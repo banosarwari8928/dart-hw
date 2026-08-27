533445a806a9b8f242869cba8454b3c7005d9dcc
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-27 13:08:24 +0430

    class

--- app.dart
+++ app.dart
@@ -1,25 +1,8 @@
 void main(){
-   Map<String,num> Students={
-    "Ali":50,
-    "Murtaza":60,
-    "Mujtaba":76,
-    "Mohammad":88,
-    "Akbar":92,
-    "Salim":98
-   };
-//    print(Students["Murtaza"]);
-print(Students.length);
-print(Students.keys);
-print(Students.values);
-Students["Hamid"]=60;
-print(Students);
-// Students.addAll=({"Dawood":78,"Karim":20});
-Students.putIfAbsent("Mamood",()=>40);
-print(Students);
-Students.update("Ali",(v)=>v-10);
-print(Students);
-
-
-Students.removeWhere((k,v)=>k.contains("m"));
-print(Students);
+  class Animal{
+   String name;
+   String type;
+   String color;
+   Animal (this.name,this.type,this.colors);
+  }
    }
