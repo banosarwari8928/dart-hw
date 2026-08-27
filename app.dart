746616d1250cdb318889adecb263e70e757c983a
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-27 13:10:11 +0430

    class

--- app.dart
+++ app.dart
@@ -1,8 +1,10 @@
-void main(){
   class Animal{
    String name;
    String type;
    String color;
    Animal (this.name,this.type,this.colors);
   }
+void main(){
+   var dog=new Animal("Dog","Wild","Brown");
+   print(dog.name);
    }
