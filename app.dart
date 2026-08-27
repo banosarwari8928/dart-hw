9ae5896d17837344d54a897dd002a6b545db2a52
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-27 13:19:20 +0430

    class

--- app.dart
+++ app.dart
@@ -1,10 +1,24 @@
   class Animal{
    String name;
-   String type;
+   String _type;
    String color;
-   Animal (this.name,this.type,this.colors);
+   set type(v){
+      if(v="pet"||"wild"){
+         this._type=v,
+      };
+   }
+   String get type{
+      Return this._type;
+   };
+   void bark(){
+   }
+   Animal (this.name,this.type,this.color);
   }
 void main(){
    var dog=new Animal("Dog","Wild","Brown");
-   print(dog.name);
+   dog.bark();
+   List<Animal> animals=[
+      Animal("cat","pet","black"),
+      Animal("rabbit","wild","white"),
+   ];
    }
