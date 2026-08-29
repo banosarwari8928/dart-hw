d27b1158da2f2389658fc6dc2499fd7007681dbb
banosarwari8928 <banosarwari8928@gmail.com>
2026-08-29 22:11:28 +0430

    class

--- app.dart
+++ app.dart
@@ -10,18 +10,14 @@
    String get type{
       return this._type;
    }
-   void bark(){
-      print("$this.name can bark");
-   }
    Animal (this.name,this._type,this.color);
   }
 void main(){
    var dog= Animal("Dog","Wild","Brown");
-   dog.bark();
    List<Animal> animals=[
       Animal("cat","pet","black"),
       Animal("rabbit","wild","white"),
    ];
    dog.type="animal";
-   print(dog.type);
+   print(dog);
    }
