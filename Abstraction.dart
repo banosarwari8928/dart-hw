6b7be3938b705f897d0467b566f48b20c9a4c4a3
banosarwari8928 <banosarwari8928@gmail.com>
2026-09-02 14:32:23 +0430

    absraction

--- Abstraction.dart
+++ Abstraction.dart
@@ -6,3 +6,14 @@ abstract class Machine{
     TornOf();
     Work();
 }
+
+class Computer extends Machine{
+    String color;
+     Computer (String name, String brand, this.color,);
+     @override TornOf(){
+        print('Computer torn of with a option and a button.');
+     }
+     @override Work(){
+        print('We can type in computer it names the work of computer.');
+     }
+}
